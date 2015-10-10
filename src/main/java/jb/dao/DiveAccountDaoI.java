c45b2c9d1ef49e10d73a8f6bb5b1bc7d99020479
xuwenming <252429711@qq.com>
2015-10-10 11:22:05 +0800

    操蛋

---
+++ src/main/java/jb/dao/DiveAccountDaoI.java
@@ -0,0 +1,28 @@
+﻿package jb.dao;
+
+import java.util.List;
+
+import jb.model.TdiveAccount;
+
+/**
+ * DiveAccount数据库操作类
+ * 
+ * @author John
+ * 
+ */
+public interface DiveAccountDaoI extends BaseDaoI<TdiveAccount> {
+
+	/**
+	 * 活动评论者用户
+	 * @param activityId
+	 * @return
+	 */
+	public List<TdiveAccount> getDiveAccountByComment(String activityId);
+	
+	/**
+	 * 活动报名者用户信息
+	 * @param activityId
+	 * @return
+	 */
+	public List<TdiveAccount> getDiveAccountByApply(String activityId);
+}
---
+++ src/main/java/jb/dao/DiveActivityApplyDaoI.java
@@ -0,0 +1,22 @@
+﻿package jb.dao;
+
+import java.util.HashMap;
+
+import jb.model.TdiveActivityApply;
+
+/**
+ * DiveActivityApply数据库操作类
+ * 
+ * @author John
+ * 
+ */
+public interface DiveActivityApplyDaoI extends BaseDaoI<TdiveActivityApply> {
+	
+	/**
+	 * 统计申请数
+	 * @param activityIds
+	 * @return
+	 */
+	public HashMap<String,Integer> getCountApplyNum(String... activityIds);
+
+}
